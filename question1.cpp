88be07010c234111b03b6be5afcb68f1fab655b9
Addya Pandey <123183433+AddyaPandey@users.noreply.github.com>
2024-07-07 21:30:42 +0530

    question1

---
+++ question1.cpp
@@ -0,0 +1,22 @@
+/*
+
+ ****
+ ****          solve this pattern
+ ****
+ ****
+
+ */
+#include <iostream>
+using namespace std;
+int main()
+{
+    for (int i = 1; i <= 4; i++)
+    {
+        for (int j = 1; j <= 4; j++)
+        {
+            cout<<"*";
+        }
+            cout<<endl;
+    }
+    return 0;
+}
