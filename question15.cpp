21283cb9e2bfa532e337d3880b9e554dfbb10480
Addya Pandey <123183433+AddyaPandey@users.noreply.github.com>
2024-07-15 20:03:43 +0530

    medium

---
+++ question15.cpp
@@ -0,0 +1,25 @@
+/*
+
+EEEEE
+DDDDD
+CCCCC
+BBBBB
+AAAAA
+
+ */
+#include <iostream>
+using namespace std;
+
+int main()
+{
+ 
+    for (char i = 'E'; i >= 'A'; i--)
+    {
+        for (char j = 'E'; j >= 'A'; j--)
+        {
+            cout<< i <<" ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
