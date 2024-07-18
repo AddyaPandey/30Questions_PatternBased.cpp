e643c63b69011440d025a148ca92666ca9f3260e
Addya Pandey <123183433+AddyaPandey@users.noreply.github.com>
2024-07-18 21:44:58 +0530

    hard

--- question19.cpp
+++ question19.cpp
@@ -9,15 +9,25 @@ ABCDCBA
 #include <iostream>
 using namespace std;
 
-
 int main()
-{ int n;
-cin>> n;
-    for (char i = 1; i <= n; i++)
+{
+    for (char i = 1; i <= 5; i++)
     {
-        for (char j = 'A'; j <= 'A' + n- i; j++)
+        for (int j = 1; j <= 5 - i; j++)
         {
-            cout << j << " ";
+            cout << " ";
+        }
+        char ch = 'A';
+        int breakpoint = 2 * i - 1 / 2;
+        for (int j = 1; j <= 2 * i - 1; j++)
+        {
+            cout << ch;
+            if (j <= breakpoint) ch++;
+            else ch--;
+        }
+        for (int j = 1; j <= 5 - i; j++)
+        {
+            cout << " ";
         }
         cout << endl;
     }
