904de011e3618ebee5d29cfc9474b0a32d54a711
Addya Pandey <123183433+AddyaPandey@users.noreply.github.com>
2024-07-19 16:55:07 +0530

    hard

---
+++ question20.cpp
@@ -0,0 +1,24 @@
+/*
+
+A
+DE
+CDE
+BCDE
+ABCDE
+
+ */
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for (int i = 0; i < 5; i++)
+    {
+        for (char ch = 'E' - i; ch <= 'E'; ch++)
+        {
+            cout << ch << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
